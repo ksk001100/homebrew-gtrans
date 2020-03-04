8056419d5a39f14b8091b4192dbec6733510342a
ksk001100 <hm.pudding0715@gmail.com>
2020-03-04 23:52:12 +0900

    v0.2.1

--- gtrans.rb
+++ gtrans.rb
@@ -1,11 +1,11 @@
 require "formula"
 
-HOMEBREW_GTRANS_VERSION="0.2.0"
+HOMEBREW_GTRANS_VERSION="0.2.1"
 
 class Gtrans < Formula
   url "https://github.com/ksk001100/gtrans/releases/download/v#{HOMEBREW_GTRANS_VERSION}/gtrans-v#{HOMEBREW_GTRANS_VERSION}-x86_64-apple-darwin.zip"
   homepage "https://github.com/ksk001100/gtrans"
-  sha256 "e2a4c545215d6b92ec29b453aa38624994dab00eb2815467880f608f12543ae5"
+  sha256 "78465363c3a59fce18c76dfc2316eb353b74a3a1dfd8b5f1cc7d828d567788f8"
 
   version "v#{HOMEBREW_GTRANS_VERSION}"
   head "https://github.com/ksk001100/gtrans.git", :branch => "master"
